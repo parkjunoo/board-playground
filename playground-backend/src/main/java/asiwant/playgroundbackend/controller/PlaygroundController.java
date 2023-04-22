c5035bc569b1a88ce5700c6b1a48418198d44fb0
parkjunsoo <01junsoo@gmail.com>
2023-04-22 20:52:40 +0900

    feat: 구조위치변경

--- playground-backend/src/main/java/asiwant/playgroundbackend/controller/PlaygroundController.java
+++ playground-backend/src/main/java/asiwant/playgroundbackend/controller/PlaygroundController.java
@@ -1,11 +1,13 @@
-package controller;
+package asiwant.playgroundbackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PlaygroundController {
-	@GetMapping("/")
+	@GetMapping("/item")
 	public String home() {
 		System.out.println("하이하");
 		return "데이터 준비중 ...";
